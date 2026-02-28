0d6d8269df12fb922d6f268bb35a6abc138ac91c
Ajay P <ajayrspa82@gmail.com>
2026-02-28 18:29:13 +0530

    Initial commit

---
+++ src/test/java/com/wipro/amazecare/AmazecareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.amazecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazecareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
